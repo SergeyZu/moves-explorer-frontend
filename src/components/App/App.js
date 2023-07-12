bb62c1b83ec5db6dba26565a9da4d8e4867d26b2
SergeyZu <vechniystudent19@yandex.ru>
2023-07-12 21:33:13 +0700

    Устранил горизонтальный скролл в компоненте Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,22 @@
 import './App.css'
-// import Main from '../Main/Main'
+import Main from '../Main/Main'
 // import Movies from '../Movies/Movies'
 // import SavedMovies from '../SavedMovies/SavedMovies'
 // import Profile from '../Profile/Profile'
 // import Register from '../Register/Register'
 // import Login from '../Login/Login'
-import PageNotFound from '../PageNotFound/PageNotFound'
+// import PageNotFound from '../PageNotFound/PageNotFound'
 
 function App() {
   return (
     <div className='app'>
-      {/* <Main /> */}
+      <Main />
       {/* <Movies /> */}
       {/* <SavedMovies /> */}
       {/* <Profile /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <PageNotFound />
+      {/* <PageNotFound /> */}
     </div>
   )
 }
--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -1,6 +1,5 @@
 import './HeaderMain.css'
 import Logo from '../Buttons/LogoButton/LogoButton'
-// import RegisterLink from '../Buttons/RegisterLink/RegisterLink'
 import LoginMainButton from '../Buttons/LoginMainButton/LoginMainButton'
 
 function HeaderMain() {
@@ -11,7 +10,6 @@ function HeaderMain() {
         <a className='header-main__link' href='#'>
           Регистрация
         </a>
-        {/* <RegisterLink /> */}
         <LoginMainButton />
       </div>
     </header>

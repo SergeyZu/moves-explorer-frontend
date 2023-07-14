6382d898f2959bb2ac9c4ad9f556a672acc4db98
SergeyZu <vechniystudent19@yandex.ru>
2023-07-14 22:30:09 +0700

    Изменил точки перелома для Moves

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -4,19 +4,60 @@ import words from '../../images/33words.png'
 
 function MoviesCard() {
   return (
-    <li className='movies-card'>
-      <img className='movies-card__image' src={words} alt='Кадр из фильма' />
-      <div className='movies-card__info'>
-        {/* <div className='movies-card__row'> */}
-        <h2 className='movies-card__title'>33 слова о дизайне</h2>
-        <div className='movies-card__like'>
-          <LikeButton />
-        </div>
+    <>
+      <li className='movies-card'>
+        <img className='movies-card__image' src={words} alt='Кадр из фильма' />
+        <div className='movies-card__info'>
+          {/* <div className='movies-card__row'> */}
+          <h2 className='movies-card__title'>33 слова о дизайне</h2>
+          <div className='movies-card__like'>
+            <LikeButton />
+          </div>
 
-        {/* </div> */}
-        <p className='movies-card__duration'>1ч 47м</p>
-      </div>
-    </li>
+          {/* </div> */}
+          <p className='movies-card__duration'>1ч 47м</p>
+        </div>
+      </li>
+      <li className='movies-card'>
+        <img className='movies-card__image' src={words} alt='Кадр из фильма' />
+        <div className='movies-card__info'>
+          {/* <div className='movies-card__row'> */}
+          <h2 className='movies-card__title'>33 слова о дизайне</h2>
+          <div className='movies-card__like'>
+            <LikeButton />
+          </div>
+
+          {/* </div> */}
+          <p className='movies-card__duration'>1ч 47м</p>
+        </div>
+      </li>
+      <li className='movies-card'>
+        <img className='movies-card__image' src={words} alt='Кадр из фильма' />
+        <div className='movies-card__info'>
+          {/* <div className='movies-card__row'> */}
+          <h2 className='movies-card__title'>33 слова о дизайне</h2>
+          <div className='movies-card__like'>
+            <LikeButton />
+          </div>
+
+          {/* </div> */}
+          <p className='movies-card__duration'>1ч 47м</p>
+        </div>
+      </li>
+      <li className='movies-card'>
+        <img className='movies-card__image' src={words} alt='Кадр из фильма' />
+        <div className='movies-card__info'>
+          {/* <div className='movies-card__row'> */}
+          <h2 className='movies-card__title'>33 слова о дизайне</h2>
+          <div className='movies-card__like'>
+            <LikeButton />
+          </div>
+
+          {/* </div> */}
+          <p className='movies-card__duration'>1ч 47м</p>
+        </div>
+      </li>
+    </>
   )
 }
 

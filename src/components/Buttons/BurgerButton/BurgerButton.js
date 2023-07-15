2f46bba66b0deeb2b81b53413086ef88f8ce1afb
SergeyZu <vechniystudent19@yandex.ru>
2023-07-15 19:36:19 +0700

    Добавил кнопки Burger и Close

---
+++ src/components/Buttons/BurgerButton/BurgerButton.js
@@ -0,0 +1,8 @@
+import './BurgerButton.css'
+import burger from '../../../images/burger.svg'
+
+function BurgerButton() {
+  return <img className='burger-button' src={burger} alt='Кнопка' />
+}
+
+export default BurgerButton
---
+++ src/components/Buttons/CloseButton/CloseButton.js
@@ -0,0 +1,8 @@
+import './CloseButton.css'
+import close from '../../../images/close.svg'
+
+function CloseButton() {
+  return <img className='close-button' src={close} alt='Кнопка' />
+}
+
+export default CloseButton

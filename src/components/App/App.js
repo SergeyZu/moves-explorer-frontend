cf36449ee926d855fec985a57d04720a91324832
SergeyZu <vechniystudent19@yandex.ru>
2023-07-18 13:51:28 +0700

    Добавил роуты в App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
-// import Main from '../Main/Main'
+import Main from '../Main/Main'
 import Movies from '../Movies/Movies'
-// import SavedMovies from '../SavedMovies/SavedMovies'
-// import Profile from '../Profile/Profile'
-// import Register from '../Register/Register'
-// import Login from '../Login/Login'
-// import PageNotFound from '../PageNotFound/PageNotFound'
+import SavedMovies from '../SavedMovies/SavedMovies'
+import Profile from '../Profile/Profile'
+import Register from '../Register/Register'
+import Login from '../Login/Login'
+import PageNotFound from '../PageNotFound/PageNotFound'
 
 function App() {
   return (
     <div className='app'>
-      {/* <Main /> */}
-      <Movies />
-      {/* <SavedMovies /> */}
-      {/* <Profile /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <PageNotFound /> */}
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+        <Route path='/*' element={<PageNotFound />} />
+      </Routes>
     </div>
   )
 }

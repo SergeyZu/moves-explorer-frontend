82f8e8afc9ffe1d371ab5375efcb9656d9f25f2f
SergeyZu <vechniystudent19@yandex.ru>
2023-07-19 16:24:38 +0700

    Изменил компонент Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,11 +1,14 @@
+import React from 'react'
 import './Preloader.css'
 
-function Preloader() {
-  return (
-    <div className='preloader-container'>
-      <span className='preloader'></span>
-    </div>
-  )
-}
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
+        </div>
+    )
+};
 
 export default Preloader

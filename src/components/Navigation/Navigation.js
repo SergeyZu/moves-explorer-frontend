a90f3d0765a788deafeffedb06b7551bdaa071cf
SergeyZu <vechniystudent19@yandex.ru>
2023-07-19 08:44:20 +0700

    Текущее сохранение

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,18 +12,24 @@ function Navigation() {
     <nav className='navigation'>
       <div className='navigation__container'>
         <div className='navigation__box'>
+          <ul
+            className={
+              isOpen
+                ? 'navigation__list navigation__list_active'
+                : 'navigation__list'
+            }
+          >
+            <li className='navigation__list-item'>Главная</li>
+            <li className='navigation__list-item'>Фильмы</li>
+            <li className='navigation__list-item'>Сохраненные фильмы</li>
+            <li className='navigation__list-item'>Аккаунт</li>
+          </ul>
           <div
             className='navigation__burger'
             onClick={() => setIsOpen(!isOpen)}
           >
             {isOpen ? <CloseButton /> : <BurgerButton />}
           </div>
-          <ul className='navigation__list'>
-            <li className='navigation__list-item'>Главная</li>
-            <li className='navigation__list-item'>Фильмы</li>
-            <li className='navigation__list-item'>Сохраненные фильмы</li>
-            <li className='navigation__list-item'>Аккаунт</li>
-          </ul>
         </div>
       </div>
     </nav>

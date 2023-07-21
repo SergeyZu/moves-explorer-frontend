9ce53cb62b3ab4e52f946f5d196b84fe1237d368
SergeyZu <vechniystudent19@yandex.ru>
2023-07-22 01:58:51 +0700

    Исправил компонент LogoButton

--- src/components/Buttons/LogoButton/LogoButton.jsx
+++ src/components/Buttons/LogoButton/LogoButton.jsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom'
 
 import './LogoButton.css'
-import logo from '../../../images/logo.svg'
 
 function LogoButton() {
   return (
     <Link to='/'>
-      <img className='logo-button' src={logo} alt='Логотип' />
+      <button className='logo-button' />
     </Link>
   )
 }

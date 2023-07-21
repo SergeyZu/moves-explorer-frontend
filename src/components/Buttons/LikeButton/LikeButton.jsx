0b85fb79a161db1b4cba642fd1e04559c6aafa69
SergeyZu <vechniystudent19@yandex.ru>
2023-07-22 01:47:26 +0700

    Удалил компонент LikeButton

--- src/components/Buttons/LikeButton/LikeButton.jsx
+++
@@ -1,8 +0,0 @@
-import like from '../../../images/like.svg'
-import './LikeButton.css'
-
-function LikeButton() {
-  return <img className='like-button' src={like} alt='Мне нравится' />
-}
-
-export default LikeButton
--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,5 +1,4 @@
 import './MoviesCard.css'
-import LikeButton from '../Buttons/LikeButton/LikeButton'
 import words from '../../images/33words.png'
 
 function MoviesCard() {
@@ -8,39 +7,8 @@ function MoviesCard() {
       <li className='movies-card'>
         <img className='movies-card__image' src={words} alt='Кадр из фильма' />
         <div className='movies-card__info'>
-          {/* <div className='movies-card__row'> */}
           <h2 className='movies-card__title'>33 слова о дизайне</h2>
-          <div className='movies-card__like'>
-            <LikeButton />
-          </div>
-
-          {/* </div> */}
-          <p className='movies-card__duration'>1ч 47м</p>
-        </div>
-      </li>
-      <li className='movies-card'>
-        <img className='movies-card__image' src={words} alt='Кадр из фильма' />
-        <div className='movies-card__info'>
-          {/* <div className='movies-card__row'> */}
-          <h2 className='movies-card__title'>33 слова о дизайне</h2>
-          <div className='movies-card__like'>
-            <LikeButton />
-          </div>
-
-          {/* </div> */}
-          <p className='movies-card__duration'>1ч 47м</p>
-        </div>
-      </li>
-      <li className='movies-card'>
-        <img className='movies-card__image' src={words} alt='Кадр из фильма' />
-        <div className='movies-card__info'>
-          {/* <div className='movies-card__row'> */}
-          <h2 className='movies-card__title'>33 слова о дизайне</h2>
-          <div className='movies-card__like'>
-            <LikeButton />
-          </div>
-
-          {/* </div> */}
+          <button className='movies-card__like' />
           <p className='movies-card__duration'>1ч 47м</p>
         </div>
       </li>

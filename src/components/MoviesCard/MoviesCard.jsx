28b9344e5a105d1d08560d97a95c9f3534c34b88
SergeyZu <vechniystudent19@yandex.ru>
2023-07-22 11:45:36 +0700

    Элемент like сделал с помощью чекбокса

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import './MoviesCard.css'
+import like from '../../images/like.svg'
+import likeoff from '../../images/likeoff.svg'
 import words from '../../images/33words.png'
 
 function MoviesCard() {
+  const [isLiked, setIsLiked] = useState(false)
+
   return (
     <>
       <li className='movies-card'>
         <img className='movies-card__image' src={words} alt='Кадр из фильма' />
         <div className='movies-card__info'>
           <h2 className='movies-card__title'>33 слова о дизайне</h2>
-          <button className='movies-card__like' />
+          <checkbox className='movies-card__like'>
+            <img
+              className='movies-card__like-image'
+              onClick={() => setIsLiked(!isLiked)}
+              src={isLiked ? like : likeoff}
+              alt='Переключатель'
+            />
+          </checkbox>
           <p className='movies-card__duration'>1ч 47м</p>
         </div>
       </li>

4bcd9784633fc9a4c37120d54ad88226df9201d2
SergeyZu <vechniystudent19@yandex.ru>
2023-07-22 10:49:03 +0700

    Исправил компонент Profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,6 @@ import Main from '../Main/Main'
 import Movies from '../Movies/Movies'
 import SavedMovies from '../SavedMovies/SavedMovies'
 import Profile from '../Profile/Profile'
-import ProfileSave from '../Profile/ProfileSave'
 import Register from '../Register/Register'
 import Login from '../Login/Login'
 import PageNotFound from '../PageNotFound/PageNotFound'
@@ -18,7 +17,6 @@ function App() {
         <Route path='/movies' element={<Movies />} />
         <Route path='/saved-movies' element={<SavedMovies />} />
         <Route path='/profile' element={<Profile />} />
-        <Route path='/profile-save' element={<ProfileSave />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<Register />} />
         <Route path='/*' element={<PageNotFound />} />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Profile.css'
 import Header from '../Header/Header'
 
 function Profile() {
+  const [isEditPushed, setisEditPushed] = useState(false)
+
+  const handleEditButtonClick = () => {
+    setisEditPushed({ isEditPushed: true })
+  }
   return (
     <>
       <Header />
@@ -21,18 +27,33 @@ function Profile() {
               <p className='profile__input'>pochta@yandex.ru</p>
             </label>
           </div>
-          <div className='profile__links'>
-            <Link
-              to='/profile-save'
-              className='profile__link profile__link_edit'
-            >
-              Редактировать
-            </Link>
-
-            <Link to='/signin' className='profile__link profile__link_logout'>
-              Выйти из аккаунта{' '}
-            </Link>
-          </div>
+          {!isEditPushed ? (
+            <div className='profile__links'>
+              {/* <Link
+                to='/profile-save'
+                className='profile__link profile__link_edit'
+              >
+                Редактировать
+              </Link> */}
+              <button
+                className='profile__link profile__link_edit'
+                type='button'
+                onClick={handleEditButtonClick}
+              >
+                Редактировать
+              </button>
+              <Link to='/signin' className='profile__link profile__link_logout'>
+                Выйти из аккаунта{' '}
+              </Link>
+            </div>
+          ) : (
+            <div className='profile__button-block'>
+              <span className='profile__span'>
+                При обновлении профиля произошла ошибка
+              </span>
+              <button className='profile__save-button'>Сохранить</button>
+            </div>
+          )}
         </section>
       </main>
     </>

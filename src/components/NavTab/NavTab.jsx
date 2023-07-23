e93f9a76be8765720d91a071278a4086a94cbcff
SergeyZu <vechniystudent19@yandex.ru>
2023-07-23 12:14:18 +0700

    Поправил NavTab

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -4,16 +4,13 @@ function NavTab() {
   return (
     <nav className='navtab'>
       <a className='navtab__anchor' href='#about-project'>
-        О проекте
-        {/* <button className='navtab__button'>О проекте</button> */}
+        <button className='navtab__button'>О проекте</button>
       </a>
       <a className='navtab__anchor' href='#techs'>
-        Технологии
-        {/* <button className='navtab__button'>Технологии</button> */}
+        <button className='navtab__button'>Технологии</button>
       </a>
       <a className='navtab__anchor' href='#student'>
-        Студент
-        {/* <button className='navtab__button'>Студент</button> */}
+        <button className='navtab__button'>Студент</button>
       </a>
     </nav>
   )

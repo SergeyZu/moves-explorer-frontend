9b262599eb81b72aeae8e616bb77b5e46dc8b1be
SergeyZu <vechniystudent19@yandex.ru>
2023-07-26 21:08:06 +0700

    изменил <img> на <button> у кнопки лайка

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './MoviesCard.css'
-import like from '../../images/like.svg'
-import likeoff from '../../images/likeoff.svg'
+// import like from '../../images/like.svg'
+// import likeoff from '../../images/likeoff.svg'
 
 function MoviesCard(movieData) {
   const { src, title, duration } = movieData
@@ -22,14 +22,27 @@ function MoviesCard(movieData) {
           {/* <h2 className='movies-card__title'>{movie.title}</h2> */}
           <h2 className='movies-card__title'>{title}</h2>
 
-          <div className='movies-card__like'>
+          {/* <div className='movies-card__like'>
             <img
               className='movies-card__like-image'
               onClick={() => setIsLiked(!isLiked)}
               src={isLiked ? like : likeoff}
               alt='Переключатель'
             />
+          </div> */}
+
+          <div className='movies-card__like'>
+            <button
+              className={
+                isLiked
+                  ? 'movies-card__like-button_clicked'
+                  : 'movies-card__like-button'
+              }
+              type='button'
+              onClick={() => setIsLiked(!isLiked)}
+            />
           </div>
+
           {/* <p className='movies-card__duration'>{movie.duration}</p> */}
           <p className='movies-card__duration'>{duration}</p>
         </div>

228395ccfffc1a3f8fdbc01cb072edef46b0c8ed
SergeyZu <vechniystudent19@yandex.ru>
2023-08-01 11:34:50 +0700

    Настроил редактирование профиля, настроил logOut

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Profile.css'
+import useForm from '../../hooks/useForm'
 import Header from '../Header/Header'
 
-function Profile({ userData }) {
+function Profile({ userData, updateUserInfo, errorMessage, logOut }) {
+  const { form, errors, hadleChange } = useForm({
+    name: '',
+    email: '',
+  })
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    updateUserInfo(form)
+  }
+
   const [isEditPushed, setisEditPushed] = useState(false)
 
   const handleEditButtonClick = () => {
@@ -36,41 +47,56 @@ function Profile({ userData }) {
                 >
                   Редактировать
                 </button>
-                <Link
-                  to='/signin'
-                  className='profile__link profile__link_logout'
+                {/* <Link
+                  to='/'
+                  className='profile__link profile__link_logout '
+                  logOut={logOut}
                 >
                   Выйти из аккаунта{' '}
-                </Link>
+                </Link> */}
+                <p
+                  className='profile__link profile__link_logout '
+                  onClick={logOut}
+                >
+                  Выйти из аккаунта{' '}
+                </p>
               </div>
             </>
           ) : (
-            <>
+            <form onSubmit={handleSubmit}>
               <div className='profile__input-block'>
                 <label className='profile__label'>
                   Имя
                   <input
                     className='profile__input'
                     type='text'
+                    id='name'
+                    name='name'
+                    value={form.name}
+                    onChange={hadleChange}
                     placeholder='Введите ваше имя'
                     required
                   />
                 </label>
+                <span className='profile__input-error'>{errors.name}</span>
                 <label className='profile__label'>
                   E-mail
                   <input
                     className='profile__input'
                     type='email'
+                    id='email'
+                    name='email'
+                    value={form.email}
+                    onChange={hadleChange}
                     placeholder='example@example.net'
                     required
                   />
                 </label>
+                <span className='profile__input-error'>{errors.email}</span>
               </div>
-              <span className='profile__span'>
-                При обновлении профиля произошла ошибка
-              </span>
+              <span className='profile__span'>{errorMessage}</span>
               <button className='profile__save-button'>Сохранить</button>
-            </>
+            </form>
           )}
         </section>
       </main>
